afa7f0d898fe3c61b6199102f22a9ace6a3cd384
Pazzo97 <pazzomuk@gmail.com>
2022-08-11 19:11:45 +0200

    validations and model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true
+  validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
