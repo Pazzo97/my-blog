f2fdf251c3114000ce56bbbad260b31300d577fb
Pazzo97 <pazzomuk@gmail.com>
2022-08-11 22:41:21 +0200

    validations and model specs edits

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,6 @@ class Post < ApplicationRecord
   end
 
   def most_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :DESC).limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :DESC).limit(3)
   end
 end
