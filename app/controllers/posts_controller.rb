1306e1fa77bf35233418169b37c9daaebe15d105
Pazzo97 <pazzomuk@gmail.com>
2022-08-26 18:46:02 +0200

    Fix linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order(created_at: :desc).includes(:comments, :likes)
