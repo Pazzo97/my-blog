9382b0d2413a25d57f54f0bee3fcdc683970abe9
Theophileaseh <theophilenwachan@gmail.com>
2022-08-27 19:38:20 +0100

    Add api for comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::CommentsController < ApplicationController
+  protect_from_forgery with: :null_session
+  def new
+    @comment = Comment.new
+    respond_to do |format|
+      format.json { render json: @comment }
+    end
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(post_id: @post.id, author_id: current_user.id, Text: comment_params)
+    @comment.post_id = @post.id
+    return unless
+    @comment.save
+
+    respond_to do |format|
+      format.json { render json: @comment, status: 'Created' }
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
+  end
+end
